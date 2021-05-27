028e95559fcb5c3e6372f96a272feae2fca128e3
Gabriel Prates <gabsprates@gmail.com>
2021-05-27 12:32:32 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 update_upgrade() {
-    echo '# Updating SO'
+    echo "# Updating SO"
     sudo apt-get update && sudo apt-get upgrade -y
     return $?
 }
 
 setup_build_essentials() {
-    echo '\n# Module: build-essential'
+    echo "\n# Module: build-essential"
     
     which make > /dev/null
     has_make=$?
@@ -17,27 +17,27 @@ setup_build_essentials() {
 
     if [ $has_make -eq 0 ] && [ $has_gcc -eq 0 ]
     then
-        echo '# Installed [OK]'
+        echo "# Installed [OK]"
         return 0
     fi
 
-    echo '# Installing...'
+    echo "# Installing..."
     sudo apt-get install build-essential -y
     return $?
 }
 
 setup_git() {
-    echo '\n# Module: git'
+    echo "\n# Module: git"
     
     which git > /dev/null
 
     if [ $? -eq 0 ]
     then
-        echo '# Installed [OK]'
+        echo "# Installed [OK]"
         return 0
     fi
 
-    echo '# Installing...'
+    echo "# Installing..."
     sudo apt-get install git -y
     return $?
 }
