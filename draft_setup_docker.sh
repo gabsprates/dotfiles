8777b7a08e12324665eb046a3edcb07a97ca113a
Gabriel Prates <gabsprates@gmail.com>
2021-05-29 18:02:16 -0300

    feat: add setup docker

---
+++ draft_setup_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "# Module: docker"
+
+which docker > /dev/null
+
+if [ $? -gt 0 ]
+then
+    echo "# Installing..."
+    wget -q -O- https://get.docker.com | sh
+
+    install_return=$?
+    if [ $install_return -gt 0 ]
+    then
+        echo "# error"
+        return $install_return
+    fi
+
+
+    echo
+    echo "# Setup user..."
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
+    docker run hello-world
+else
+    echo "# Installed [OK]"
+fi
