55e41ef64f44ccdbf9912079b36541e711935369
Gabriel Prates <gabsprates@gmail.com>
2021-06-07 19:54:38 -0300

    Update setup_gnome.sh

--- setup_gnome.sh
+++ setup_gnome.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# Appearance
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 16
-gsettings set org.gnome.shell.extensions.desktop-icons show-home false
-gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+setup_appearance() {
+  gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
+  gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 16
+  gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+  gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+}
 
-# Top bar
-gsettings set org.gnome.desktop.interface clock-show-seconds true
+setup_top_bar() {
+  gsettings set org.gnome.desktop.interface clock-show-seconds true
+}
 
-# Nautilus
-gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-gsettings set org.gnome.nautilus.list-view use-tree-view true
-gsettings set org.gnome.nautilus.list-view default-zoom-level 'small'
+setup_nautilus() {
+  gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+  gsettings set org.gnome.nautilus.list-view use-tree-view true
+  gsettings set org.gnome.nautilus.list-view default-zoom-level 'small'
+}
+
+
+case $1 in
+  
+setup_appearance
+setup_top_bar
+setup_nautilus
