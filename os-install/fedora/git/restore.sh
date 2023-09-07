fd3f633cf86da1adf8d27b06dacb8e69315485c3
Gabriel Prates <gabsprates@gmail.com>
2023-09-07 23:07:57 +0100

    feat: Add lazygit

--- os-install/fedora/git/restore.sh
+++ os-install/fedora/git/restore.sh
@@ -6,7 +6,13 @@ source ../../../global-vars.sh
 source ../../../common.sh
 
 install() {
-    dnf -qy install git
+    is_lazygit_repo_enabled=$(dnf copr list | grep "atim/lazygit")
+
+    if [ ! "$is_lazygit_repo_enabled" ]; then
+        dnf copr enable atim/lazygit -y
+    fi
+
+    dnf -qy install git lazygit
 }
 
 apply_user_settings() {
