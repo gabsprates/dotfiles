75f2f749c1665eed40bd0fa30dbaf7be7271507f
Gabriel Prates <gabsprates@gmail.com>
2023-11-22 08:52:25 -0300

    refactor: Setup to support multiple OS

--- setup.sh
+++ setup.sh
@@ -1,109 +1,54 @@
 #!/bin/bash
 
-update_upgrade() {
-    echo "# Updating SO"
-    sudo apt-get update && sudo apt-get upgrade -y
-    return $?
+update_os() {
+	echo "# Updating SO"
+
+	case "$1" in
+		fedora)
+			sudo dnf upgrade -y
+		;;
+	esac
+
+	return $?
 }
 
-setup_build_essentials() {
-    echo
-    echo "=========================="
-    echo "# Module: build-essential"
-    
-    which make > /dev/null
-    has_make=$?
+install_dependencies() {
+	echo "# Install dependencies"
 
-    which gcc > /dev/null
-    has_gcc=$?
+	case "$1" in
+		fedora)
+			sudo dnf install curl git vim -y
+		;;
+	esac
 
-    if [ $has_make -eq 0 ] && [ $has_gcc -eq 0 ]
-    then
-        echo "# Installed [OK]"
-        return 0
-    fi
-
-    echo "# Installing..."
-    sudo apt-get install build-essential -y
-    return $?
+	return $?
 }
 
-setup_vim() {
-    echo
-    echo "=========================="
-    echo "# Module: vim"
-    
-    which vim > /dev/null
-
-    if [ $? -eq 0 ]
-    then
-        echo "# Installed [OK]"
-        return 0
-    fi
-
-    echo "# Installing..."
-    sudo apt-get install vim -y
-    return $?
+clone_repo() {
+	if [ -d /home/me/.dotfiles ]
+	then
+	    echo
+	    echo "# .dotfiles [OK]"
+	else
+	    echo
+	    echo "# Cloning .dotfiles"
+	    git clone https://github.com/gabsprates/dotfiles.git /home/me/.dotfiles
+	fi
 }
 
-setup_git() {
-    echo
-    echo "=========================="
-    echo "# Module: git"
-    
-    which git > /dev/null
+main() {
+	os="$(. /etc/os-release && echo "$ID")"
 
-    if [ $? -eq 0 ]
-    then
-        echo "# Installed [OK]"
-        return 0
-    fi
+	echo "# Setting up: $os"
+	echo
 
-    echo "# Installing..."
-    sudo apt-get install git -y
-    return $?
+	update_os $os
+
+	if [ $? -eq 0 ]; then install_dependencies $os; fi
+	if [ $? -eq 0 ]; then clone_repo $os; fi
+
+	echo
+	echo "# Setup preparation done!"
 }
 
-setup_curl() {
-    echo
-    echo "=========================="
-    echo "# Module: curl"
-    
-    which curl > /dev/null
-
-    if [ $? -eq 0 ]
-    then
-        echo "# Installed [OK]"
-        return 0
-    fi
-
-    echo "# Installing..."
-    sudo apt-get install curl -y
-    return $?
-}
-
-# Updating SO
-update_upgrade
-
-# Setup packages
-if [ $? -eq 0 ]; then setup_build_essentials; fi
-if [ $? -eq 0 ]; then setup_vim; fi
-if [ $? -eq 0 ]; then setup_git; fi
-if [ $? -eq 0 ]; then setup_curl; fi
-
-# Cloning .dotfiles
-if [ -d ~/.dotfiles ]
-then
-    echo
-    echo "=========================="
-    echo "# .dotfiles [OK]"
-else
-    echo
-    echo "=========================="
-    echo "# Cloning .dotfiles"
-    git clone https://github.com/gabsprates/dotfiles.git ~/.dotfiles
-    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-fi
-
-echo 
-echo "# Setup done!"
+main
