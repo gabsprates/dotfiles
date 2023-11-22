38ff33206286ef6eda8207cb2ec277272c369d7c
Gabriel Prates <gabsprates@gmail.com>
2023-11-22 19:56:03 -0300

    refactor: Format setup.sh and change repo origin

--- setup.sh
+++ setup.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 update_os() {
-	echo "# Updating SO"
+    echo "# Updating SO"
 
-	case "$1" in
-		fedora)
-			sudo dnf upgrade -y
-		;;
-	esac
+    case "$1" in
+    fedora)
+        sudo dnf upgrade -y
+        ;;
+    esac
 
-	return $?
+    return $?
 }
 
 install_dependencies() {
-	echo "# Install dependencies"
+    echo "# Install dependencies"
 
-	case "$1" in
-		fedora)
-			sudo dnf install curl git vim -y
-		;;
-	esac
+    case "$1" in
+    fedora)
+        sudo dnf install curl git vim -y
+        ;;
+    esac
 
-	return $?
+    return $?
 }
 
 clone_repo() {
-	if [ -d /home/me/.dotfiles ]
-	then
-	    echo
-	    echo "# .dotfiles [OK]"
-	else
-	    echo
-	    echo "# Cloning .dotfiles"
-	    git clone https://github.com/gabsprates/dotfiles.git /home/me/.dotfiles
-	fi
+    if [ -d /home/me/.dotfiles ]; then
+        echo
+        echo "# .dotfiles [OK]"
+    else
+        echo
+        echo "# Cloning .dotfiles"
+        git clone https://github.com/gabsprates/dotfiles.git /home/me/.dotfiles
+        sed -i 's/https:\/\/github.com\//git@github.com:/' /home/me/.dotfiles/.git/config
+    fi
 }
 
 main() {
-	os="$(. /etc/os-release && echo "$ID")"
+    os="$(. /etc/os-release && echo "$ID")"
 
-	echo "# Setting up: $os"
-	echo
+    echo "# Setting up: $os"
+    echo
 
-	update_os $os
+    update_os $os
 
-	if [ $? -eq 0 ]; then install_dependencies $os; fi
-	if [ $? -eq 0 ]; then clone_repo $os; fi
+    if [ $? -eq 0 ]; then install_dependencies $os; fi
+    if [ $? -eq 0 ]; then clone_repo $os; fi
 
-	echo
-	echo "# Setup preparation done!"
+    echo
+    echo "# Setup preparation done!"
 }
 
 main
