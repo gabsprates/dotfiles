fb4611027e8ef427fb1c5caf07e864474322299d
Gabriel Prates <gabsprates@gmail.com>
2023-12-16 18:32:14 -0300

    refactor: Make toggle_mouse_hand sh compatible

--- scripts/toggle_mouse_hand.sh
+++ scripts/toggle_mouse_hand.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-function toggle_hand {
-	local is_left_handed=$(gsettings get org.gnome.desktop.peripherals.mouse left-handed)
-	
-	if [ $is_left_handed == 'true' ]; then
-		gsettings set org.gnome.desktop.peripherals.mouse left-handed false
-	else
-		gsettings set org.gnome.desktop.peripherals.mouse left-handed true
-	fi
+toggle_hand() {
+    is_left_handed=$(gsettings get org.gnome.desktop.peripherals.mouse left-handed)
+
+    if [ "$is_left_handed" = 'true' ]; then
+        gsettings set org.gnome.desktop.peripherals.mouse left-handed false
+    else
+        gsettings set org.gnome.desktop.peripherals.mouse left-handed true
+    fi
 }
 
 toggle_hand
