8bd167c59dcaa14bbb7008c4a08b825704600dd8
Gabriel Prates <gabsprates@gmail.com>
2024-05-23 17:01:01 +0200

    feat: Auto load ui

--- me-custom/ui/customize.sh
+++ me-custom/ui/customize.sh
@@ -5,8 +5,8 @@ cd "$(dirname "$0")" || exit
 source ../../global-vars.sh
 
 apply_settings() {
-    cat "$DOTFILES_BASE_PATH/me-custom/ui/profile"
-    # dconf load / < "$DOTFILES_BASE_PATH/me-custom/ui/profile"
+    # cat "$DOTFILES_BASE_PATH/me-custom/ui/profile"
+    dconf load / <"$DOTFILES_BASE_PATH/me-custom/ui/profile"
 }
 
 apply_settings
