0394344097eec1f50f10c521e4dfcc3b04058b52
Gabriel Prates <gabsprates@gmail.com>
2025-06-18 05:01:36 +0200

    feat: add javascript installer to python scripts

---
+++ my_app/plugins/javascript/__init__.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from app_system import AppInstaller, run_as_me
+
+
+class TerminatorInstaller(AppInstaller):
+    def __init__(self, os_id: str):
+        self.os_id = os_id
+        self.plugin_path = Path(__file__).parent.resolve()
+
+    def install(self):
+        run_as_me([
+            'asdf', 'plugin', 'add', 'nodejs', 'https://github.com/asdf-vm/asdf-nodejs.git'
+        ])
+        run_as_me([
+            'asdf', 'install', 'nodejs', '22.16'
+        ])
+        run_as_me([
+            'asdf', 'global', 'nodejs', '22.16'
+        ])
+
+    def customize(self):
+        pass
