cb4fcd26f1463db168d9bde1c1785f39158549d3
Gabriel Prates <gabsprates@gmail.com>
2025-06-24 02:45:34 +0200

    refactor(docker): change script to run as me as default

--- my_app/plugins/docker/__init__.py
+++ my_app/plugins/docker/__init__.py
@@ -10,19 +10,17 @@ from app_system import AppInstaller
 class DockerInstaller(AppInstaller):
     def __init__(self, os_id: str):
         self.os_id = os_id
-        sys.path.append(os.path.dirname(__file__))
 
     def install(self):
-        tmp_script = os.path.join(
-            tempfile.mkdtemp(), "install-docker.sh")
+        tmp_script = Path(tempfile.mkdtemp()).joinpath("install-docker.sh")
 
         urllib.request.urlretrieve("https://get.docker.com", tmp_script)
 
-        subprocess.run(["sh", tmp_script])
-        subprocess.run(["systemctl", "start", "docker"])
-        subprocess.run(["systemctl", "enable", "docker"])
-        subprocess.run(["gpasswd", "-a", "me", "docker"])
-        subprocess.run(["systemctl", "restart", "docker"])
+        subprocess.run(["sudo", "sh", tmp_script])
+        subprocess.run(["sudo", "systemctl", "start", "docker"])
+        subprocess.run(["sudo", "systemctl", "enable", "docker"])
+        subprocess.run(["sudo", "gpasswd", "-a", "me", "docker"])
+        subprocess.run(["sudo", "systemctl", "restart", "docker"])
 
     def customize(self):
         pass
