d7eb238da6ecd923f4ffe94c5bc860a1f6b3c453
Gabriel Prates <gabsprates@gmail.com>
2025-06-24 03:51:58 +0200

    refactor(brave): use Path and cleaning code

--- my_app/plugins/brave/__init__.py
+++ my_app/plugins/brave/__init__.py
@@ -1,18 +1,16 @@
-import os
 import subprocess
-import sys
-
 import urllib.request
+
+from pathlib import Path
 from app_system import AppInstaller
 
 
 class BraveInstaller(AppInstaller):
     def __init__(self, os_id: str):
         self.os_id = os_id
-        sys.path.append(os.path.dirname(__file__))
 
     def install(self):
-        tmp_install = '/tmp/brave.sh'
+        tmp_install = Path(tempfile.mkdtemp()).joinpath("install-brave.sh")
 
         urllib.request.urlretrieve(
             'https://dl.brave.com/install.sh', tmp_install)
