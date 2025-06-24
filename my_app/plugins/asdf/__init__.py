355ed706413d449a510199f7e00afdb8e0536164
Gabriel Prates <gabsprates@gmail.com>
2025-06-25 01:44:19 +0200

    refactor(asdf): use Path and cleaning code

--- my_app/plugins/asdf/__init__.py
+++ my_app/plugins/asdf/__init__.py
@@ -1,25 +1,20 @@
 import json
 import os
-import sys
 import urllib.request
 import tarfile
+import tempfile
 
+from pathlib import Path
 from app_system import AppInstaller
 
 
 class AsdfInstaller(AppInstaller):
     def __init__(self, os_id: str):
         self.os_id = os_id
-        sys.path.append(os.path.dirname(__file__))
 
     def install(self):
-        asdf_tmp = os.path.join("/tmp", "asdf.tar.gz")
-        asdf_package_url = ""
-        asdf_releases_url = "https://api.github.com/repos/asdf-vm/asdf/releases/latest"
-
-        with urllib.request.urlopen(asdf_releases_url) as response:
-            json_data = json.loads(response.read().decode())
-            asdf_package_url = self.get_asdf_linux_package_url(json_data)
+        asdf_tmp = Path(tempfile.mkdtemp()).joinpath("asdf.tar.gz")
+        asdf_package_url = self.get_asdf_linux_package_url()
 
         self.download_asdf_linux_package(asdf_package_url, asdf_tmp)
         self.install_asdf(asdf_tmp)
@@ -27,15 +22,20 @@ class AsdfInstaller(AppInstaller):
     def customize(self):
         pass
 
-    def get_asdf_linux_package_url(self, data):
-        for asset in data['assets']:
-            if asset['browser_download_url'].endswith("-linux-386.tar.gz"):
-                return asset['browser_download_url']
+    def get_asdf_linux_package_url(self):
+        asdf_releases_url = "https://api.github.com/repos/asdf-vm/asdf/releases/latest"
 
-    def download_asdf_linux_package(self, url: str, dest: str):
+        with urllib.request.urlopen(asdf_releases_url) as response:
+            json_data = json.loads(response.read().decode())
+
+            for asset in json_data['assets']:
+                if asset['browser_download_url'].endswith("-linux-386.tar.gz"):
+                    return asset['browser_download_url']
+
+    def download_asdf_linux_package(self, url: str, dest: Path):
         urllib.request.urlretrieve(url, dest)
 
-    def install_asdf(self, tar_file: str):
-        dest = '/usr/local/bin/'
+    def install_asdf(self, tar_file: Path):
+        dest = Path('/usr/local/bin/')
         with tarfile.open(tar_file, 'r') as file:
             file.extract(member='asdf', path=dest)
