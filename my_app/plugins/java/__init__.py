9b7fd62b0ee4dc552562e682d0d01f2cc0f7fdfd
Gabriel Prates <gabsprates@gmail.com>
2025-06-25 15:29:52 +0200

    refactor(java): change script to run as me as default

--- my_app/plugins/java/__init__.py
+++ my_app/plugins/java/__init__.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from pathlib import Path
 from app_system import AppInstaller
 
@@ -5,20 +7,26 @@ from app_system import AppInstaller
 class JavaInstaller(AppInstaller):
     def __init__(self, os_id: str):
         self.os_id = os_id
-        self.plugin_path = Path(__file__).parent.resolve()
 
     def install(self):
-        print("Not possible to install as `me` user. Run the following commands to install it:\n")
-        print('asdf plugin add java https://github.com/halcyon/asdf-java.git')
-        print('asdf install java latest:zulu-21')
-        print('asdf set -u java latest:zulu-21')
+        subprocess.run(['asdf', 'plugin', 'add', 'java', 'https://github.com/halcyon/asdf-java.git'])
+        subprocess.run(['asdf', 'install', 'java', 'latest:zulu-21'])
+        subprocess.run(['asdf', 'set', '-u', 'java', 'latest:zulu-21'])
 
-        print('\nAdd the following on your ~/.zsh_local file:\n')
-        print('''
+        zsh_local_path = Path('/home/me/.zsh_local')
+        file_content = ''
+
+        with open(zsh_local_path, 'r') as zsh_local:
+            file_content = zsh_local.read()
+
+        file_content = file_content.replace(
+            "#java_profile", '''
 ## Java
 [ -s "$HOME/.asdf/plugins/java/set-java-home.zsh" ] && source "$HOME/.asdf/plugins/java/set-java-home.zsh"
-'''
-              )
+''')
+
+        with open(zsh_local_path, 'w') as zsh_local:
+            zsh_local.write(file_content)
 
     def customize(self):
         pass
