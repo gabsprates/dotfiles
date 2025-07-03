d86c4ae60add2e25f391b1e355a954c82673ac2c
Gabriel Prates <gabsprates@gmail.com>
2025-07-03 02:59:24 +0200

    docs(app_system): add documentation strings

--- my_app/app_system.py
+++ my_app/app_system.py
@@ -6,17 +6,32 @@ from pathlib import Path
 
 
 class AppInstaller:
+    """
+    Interface to be implemented for each app that should be installed.
+    """
+
     def install(self):
+        """
+        Method that executes any sudo actions, as instaling packages.
+        """
         raise NotImplementedError(
             "Plugins must implement the 'install' method.")
 
     def customize(self):
+        """
+        Method that execcutes any user level customization, like editing
+        PATH, or even installing something at user's directory.
+        """
         raise NotImplementedError(
             "Plugins must implement the 'customize' method.")
 
 
 class InstallerManager:
     def __init__(self, plugin_dir: Path, os_id: str, apps: list[str]):
+        """
+        Loads plugins from the plugins directory and register them for future installation.
+        """
+
         self.plugin_dir = str(plugin_dir)
         self.os_id = os_id
         self.apps = []
@@ -43,6 +58,9 @@ class InstallerManager:
                     })
 
     def install_apps(self):
+        """
+        Runs app .install() and .customize() methods.
+        """
         print(f"## Installing apps for: {self.os_id.upper()}", '\n')
 
         for app in self.apps:
