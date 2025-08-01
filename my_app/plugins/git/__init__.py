d85134e1ee970e30c58ca0cef7e88b6ca3f650e1
Gabriel Prates <gabsprates@gmail.com>
2025-08-01 20:51:12 +0200

    fix(git): install lazygit from tar file and build-essential

--- my_app/plugins/git/__init__.py
+++ my_app/plugins/git/__init__.py
@@ -1,5 +1,9 @@
 import os
+import json
 import subprocess
+import urllib.request
+import tarfile
+import tempfile
 
 from pathlib import Path
 from app_system import AppInstaller
@@ -36,5 +40,34 @@ class GitInstaller(AppInstaller):
         subprocess.run(["sudo", "dnf", "-qy", "install", "git-lfs"])
 
     def install_on_ubuntu(self):
-        subprocess.run(["sudo", "apt", "-qy", "install", "build-essential", "git", "lazygit"])
+        subprocess.run(["sudo", "apt", "-qy", "install", "build-essential", "git"])
         subprocess.run(["curl", "-s", "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh", "|", "sudo", "bash"])
+        subprocess.run(["sudo", "apt-get", "install", "-qy", "git-lfs"])
+
+        self.install_lazygit()
+
+    def install_lazygit(self):
+        lazygit_tmp = Path(tempfile.mkdtemp())
+        lazygit_package_file = lazygit_tmp.joinpath("lazygit.tar.gz")
+        lazygit_package_url = ""
+        lazygit_releases_url = "https://api.github.com/repos/jesseduffield/lazygit/releases/latest"
+
+        with urllib.request.urlopen(lazygit_releases_url) as response:
+            json_data = json.loads(response.read().decode())
+            lazygit_package_url = self.get_lazygit_linux_package_url(json_data)
+
+        self.download_lazygit_linux_package(lazygit_package_url, lazygit_package_file)
+
+        with tarfile.open(lazygit_package_file, 'r') as file:
+            file.extract(member='lazygit', path=lazygit_tmp)
+
+        subprocess.run(['sudo', 'cp', str(lazygit_tmp.joinpath("lazygit")), '/usr/local/bin/'])
+
+    def download_lazygit_linux_package(self, url: str, lazygit_dest: Path):
+        urllib.request.urlretrieve(url, lazygit_dest)
+
+    def get_lazygit_linux_package_url(self, data):
+        for asset in data['assets']:
+            if asset['browser_download_url'].endswith("_Linux_x86_64.tar.gz"):
+                return asset['browser_download_url']
+
